f75a6b1e54a1d030b0d378dc6ddcb02d0cf959c7
OMJR72 <jarquinorlando72@gmail.com>
2026-07-27 10:29:54 -0600

    Setup cson front y back

---
+++ Backend/eq16/src/main/java/com/proyectofinal/eq16/Eq16Application.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.eq16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eq16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eq16Application.class, args);
+	}
+
+}
---
+++ Backend/eq16/src/test/java/com/proyectofinal/eq16/Eq16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.eq16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eq16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
