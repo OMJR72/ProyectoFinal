57154cbd18a831528bf0b70a96fb6e27e54952cb
OMJR72 <jarquinorlando72@gmail.com>
2026-07-28 01:54:08 -0600

    Agregando dtos para manejar el registro de usuarios

--- Backend/eq16/src/main/java/com/proyectofinal/eq16/dto/LoginRequest.java
+++ Backend/eq16/src/main/java/com/proyectofinal/eq16/dto/LoginRequest.java
@@ -2,7 +2,7 @@ package com.proyectofinal.eq16.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class RegisterRequest{
+public class LoginRequest{
 
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
@@ -12,18 +12,6 @@ public class RegisterRequest{
 
     @NotBlank(message = "El password es obligatorio")
     private String password;
-
-    @NotBlank(message = "El email es obligatorio")
-    private String email;
-
-    private Long idRol;
-
-    public RegisterRequest(String nombre, String password, String email, Long idRol) {
-        this.nombre = nombre;
-        this.password = password;
-        this.email = email;
-        this.idRol = idRol;
-    }
 
     public String getnombre() {
         return nombre;
@@ -39,22 +27,6 @@ public class RegisterRequest{
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getIdRol() {
-        return idRol;
-    }
-
-    public void setIdRol(Long idRol) {
-        this.idRol = idRol;
     }
 
     public String getApellido() {
