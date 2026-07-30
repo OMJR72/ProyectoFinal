f1f10e40eb177f67aaee62ebcc48e5fc525e0187
root <root@srv1828945.hstgr.cloud>
2026-07-30 16:33:39 +0000

    configuracion del properties

--- Frontend/frontenProyectoFinal/vite.config.js
+++ Frontend/frontenProyectoFinal/vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/ProyectoFinal/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8090",
+        changeOrigin: true,
+      },
+    },
+  },
 });
